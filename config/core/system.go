6bdf688ddb1ee3704cde1b2f8339fd71427d7621
LINQQ1212 <1142798229@qq.com>
2023-02-09 16:16:05 +0800

    Template

--- config/core/system.go
+++ config/core/system.go
@@ -12,4 +12,5 @@ type System struct {
 	TGChatId  string `mapstructure:"tg-chat-id" json:"tg-chat-id" yaml:"tg-chat-id"`
 	Info      string `mapstructure:"info" json:"info" yaml:"info"`
 	Category  bool   `mapstructure:"category" json:"category" yaml:"category"`
+	Template  bool   `mapstructure:"template" json:"template" yaml:"template"`
 }
